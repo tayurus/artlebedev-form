b94f87b157aab1a4f40ff39a67dad001860eab41
Yury Tatarintsev <yury@realrussialimited.local>
2018-08-01 11:00:21 +0300

    bug fixed: 'Button' did not get modifier class.

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Button.css";
 
 export const Button = props => {
-    return <button className={"Button " + props.type}>{props.label}</button>;
+    return <button className={"Button Button_" + props.type}>{props.label}</button>;
 };
