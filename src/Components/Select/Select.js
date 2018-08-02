219f36c2ba7a7d771fa378f3d9bd7f860f7ce97a
Yury Tatarintsev <yury@realrussialimited.local>
2018-08-02 10:28:09 +0300

    fix my dumbass-error: rename Table to Select

--- src/Components/Select/Select.js
+++ src/Components/Select/Select.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Select.css";
 
-export const Table = props => {
+export const Select = props => {
     return <select class="Select">{props.options.map((option, key) => <option value={key}>{option}</option>)}</select>;
 };
