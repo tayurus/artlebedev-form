1b48e621d249e451aadc90bc1fe86b1f49fe74ab
Yury Tatarintsev <yury@realrussialimited.local>
2018-08-02 15:55:19 +0300

    'Select' Component: fix 'class' attribute to 'className'

--- src/Components/Select/Select.js
+++ src/Components/Select/Select.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./Select.css";
 
 export const Select = props => {
-    return <select class="Select">{props.options.map((option, key) => <option value={key}>{option}</option>)}</select>;
+    return <select className="Select">{props.options.map((option, key) => <option value={key}>{option}</option>)}</select>;
 };
